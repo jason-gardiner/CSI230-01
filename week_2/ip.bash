a8fb2eafce46e82d2cdd578518514707c08190ab
Jason Gardiner <jason.gardiner@mymail.champlain.edu>
2023-09-11 18:08:29 -0400

    ip.bash implementation

--- week_2/ip.bash
+++ week_2/ip.bash
@@ -1,1 +1,6 @@
+#!/bin/bash
+
+# ip addr gets entire ip address
+# global only appears in the line with the relevent information
+# printing the second word on that line gives the required information
 ip addr | awk '/global/ {print $2}'
