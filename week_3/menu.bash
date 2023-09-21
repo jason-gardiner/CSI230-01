a1dd6406e5db336ce4ce07825be91ce054a7956c
jason-gardiner <119627004+jason-gardiner@users.noreply.github.com>
2023-09-21 13:24:07 -0400

    Update menu.bash

--- week_3/menu.bash
+++ week_3/menu.bash
@@ -3,13 +3,13 @@
 # Storyline: Menu for admin, VPN, and Security functions
 
 # Implement the following
-# DONE 1) Add another switch with an argument that checks to see if the user exists in the wg0.conf file
+# 1) Add a Check if User Exists in VPN Menu
 # DONE 2) Edit the manage-users.bash script and add the code to delete a user from the wg0.conf file using sed
 # 3) Create a menu for security admin tasks that performs the tasks:
 #   3a) List open network sockets
 #   3b) Check if any user besides root has a UID of 0
-#   3c) Check the last 10 logged in users
-#   3d) See currently logged in users
+#   DONE 3c) Check the last 10 logged in users
+#   DONE 3d) See currently logged in users
 # 4) Create a new user and add the menu as their shell
 
 function invalid_opt() {
