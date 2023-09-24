43fea7fdaf2deee772e6d8618b80c00d426d5e56
jason-gardiner <119627004+jason-gardiner@users.noreply.github.com>
2023-09-24 12:37:10 -0400

    Update parse-threat.bash

--- week_4/parse-threat.bash
+++ week_4/parse-threat.bash
@@ -5,7 +5,29 @@
 # Regex to extract the networks
 # 5.134.128.0/19
 
-# wget https://rules.emergingthreats.net/blockrules/emerging-drop.suricata.rules -O /tmp/emerging-drop.suricata.rules
+# If the file exists, don't download it
+pFile="emerging-drop.suricata.rules"
+if [[ -f "${pFile}" ]]
+then 
+	# Prompt if we need to overwrite the file
+  echo "The file ${pFile} already exists."
+	echo -n "Do you want to overwrite it? [y|n]"
+	read to_overwrite
+
+	if [[ "${to_overwrite}" == "N" || "${to_overwrite}" == "" || "${to_overwrite}" == "n"  ]]
+	then
+		echo "Exiting..."
+		exit 0
+	elif [[ "${to_overwrite}" == "y" ]]
+	then
+		echo "Creating the emerging threats file..."
+    wget https://rules.emergingthreats.net/blockrules/emerging-drop.suricata.rules -O /tmp/emerging-drop.suricata.rules
+	# If they don't specify y/N then error
+	else
+		echo "Invalid value"
+		exit 1
+	fi
+fi
 
 egrep -o '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.0/[0-9]{1,2}' /tmp/emerging-drop.suricata.rules | sort -u | tee badIPs.txt
 
