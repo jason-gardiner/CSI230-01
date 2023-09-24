9c8b72ed74cadf05af56eab6134135d504c58639
jason-gardiner <119627004+jason-gardiner@users.noreply.github.com>
2023-09-24 11:20:42 -0400

    Update parse-threat.bash

--- week_4/parse-threat.bash
+++ week_4/parse-threat.bash
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 # Stroyline: Extract IPs from emergingthreats.net and create a firewall ruleset
+
+# Regex to extract the networks
+# 5.134.128.0/19
+
+# wget https://rules.emergingthreats.net/blockrules/emerging-drop.suricata.rules -O /tmp/emerging-drop.suricata.rules
+
+egrep -o '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.0/[0-9]{1,2}' /tmp/emerging-drop.suricata.rules | sort -u | tee badIPs.txt
