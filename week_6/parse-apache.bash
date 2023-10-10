0dc490608cf189d758f6b5f2ca5f93e494fdae2b
jason-gardiner <119627004+jason-gardiner@users.noreply.github.com>
2023-10-10 12:07:12 -0400

    Update parse-apache.bash

--- week_6/parse-apache.bash
+++ week_6/parse-apache.bash
@@ -3,22 +3,22 @@
 # Storyline: Parses Apache Log for bad IP Addresses
 
 # Define the path to the Apache log file
-pFile="/access.log.txt"
-cat "$pFile"
+#pFile="access.log.txt"
+cat "parse-apache.bash"
 
 # Check if the Apache log file exists
-if [ ! -f "$pFile" ]
-then
-  echo "Apache log file not found. Exiting."
-  exit 1
-fi
+#if [ ! -f "$pFile" ]
+#then
+  #echo "Apache log file not found. Exiting."
+  #exit 1
+#fi
 
 # Parse the Apache log file to extract unique IP addresses
-badIps=$(awk '{print $1}' "$pFile" | sort -u)
+#badIps=$(awk '{print $1}' "$pFile" | sort -u)
 
 # Add IPTables rules for each IP address
-for IP in $badIps
-do
+#for IP in $badIps
+#do
   # Add the IPTables rule to block incoming traffic from the IP
-  echo "iptables -A INPUT -s $IP -j DROP" | tee -a badIPS.iptables
-done
+  #echo "iptables -A INPUT -s $IP -j DROP" | tee -a badIPS.iptables
+#done
