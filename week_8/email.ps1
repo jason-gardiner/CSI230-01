4717309137a1bd17a4731d922a08adacb7b38b43
jason-gardiner <119627004+jason-gardiner@users.noreply.github.com>
2023-10-17 17:42:28 -0400

    Add files via upload

---
+++ week_8/email.ps1
@@ -0,0 +1,16 @@
+﻿# Storyline: Send an email.
+
+# Body of the email
+# Variable can have an underscore or any alphanumeric value
+$msg = "Exterminate!"
+
+write-host -BackgroundColor Red -ForegroundColor White $msg
+
+# Email From Address
+$email = "jason.gardiner@mymail.champlain.edu"
+
+# To address
+$toEmail = "deployer@csi-web"
+
+# Sending the email
+Send-MailMessage -From $email -To $toEmail -Subject "A Greeting" -Body $msg -SmtpServer 192.168.6.71
---
+++ week_8/search-event-logs.ps1
@@ -0,0 +1,17 @@
+﻿# Storyline: Review the Security Event Log
+
+# Directory to save files:
+$myDir = "C:\Users\champuser\Desktop\CSI230-01\week_8\"
+
+# List all the available Windows Event Logs
+Get-EventLog -list
+
+# Create a prompt to allow user to select the Log to view
+$readLog = Read-host -Prompt "Please select a log to review from the list above"
+
+# Create a prompt to allow user to search the Log selected
+$searchLog = Read-host -Prompt "Please specify a keyword or phrase to search for"
+
+# Print the results for the log
+Get-EventLog -LogName $readLog -Newest 40 | where {$_.Message -ilike "*$searchLog*" } | export-csv -NoTypeInformation `
+-Path "$myDir\securityLogs.csv"
