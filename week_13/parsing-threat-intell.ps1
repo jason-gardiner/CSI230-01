bd75a3622ee1214019f011feb3a2e8893a06a2bf
Jason Gardiner <gardinerj221@gmail.com>
2023-12-05 15:56:11 -0500

    Adding switch case

--- week_13/parsing-threat-intell.ps1
+++ week_13/parsing-threat-intell.ps1
@@ -17,14 +17,14 @@ foreach ($u in $drop_urls) {
     } else {
 
         # Download the rules list
-        Invoke-WebRequest -Uri $u -OutFile $file_name
+        Invoke-WebRequest -Uri $u -OutFile "C:\Users\champuser\Desktop\CSI230-01\week_13\$file_name"
 
     } # close if statement
 
 } # close foreach loop
 
 # Array containing the filename
-$input_paths = @('.\compromised-ips.txt','.\emerging-botcc.rules')
+$input_paths = @('C:\Users\champuser\Desktop\CSI230-01\week_13\compromised-ips.txt','C:\Users\champuser\Desktop\CSI230-01\week_13\emerging-botcc.rules')
 
 # Extract the IP addresses
 $regex_drop = '\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b'
@@ -33,11 +33,40 @@ $regex_drop = '\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b'
 select-string -Path $input_paths -Pattern $regex_drop | `
 ForEach-Object { $_.Matches } | `
 ForEach-Object { $_.Value } | Sort-Object | Get-Unique | `
-Out-File -FilePath "ips-bad.tmp"
+Out-File -FilePath "C:\Users\champuser\Desktop\CSI230-01\week_13\ips-bad.tmp"
 
-# Get the IP addresses discovered, loop through and replace the beghinning of the line with the IPTables syntax
-# After the IP address, add the remaining IPTables syntax and save the results to a file
-# iptables -A INPUT -s IP -j DROP
-(Get-Content -Path ".\ips-bad.tmp") | % `
-{ $_ -replace "^","iptables -A INPUT -s " -replace "$"," -j DROP" } | `
-Out-File -FilePath "iptables.bash"
+function Get-iptables {
+    # Get the IP addresses discovered, loop through and replace the beghinning of the line with the IPTables syntax
+    # After the IP address, add the remaining IPTables syntax and save the results to a file
+    # iptables -A INPUT -s IP -j DROP
+    (Get-Content -Path "C:\Users\champuser\Desktop\CSI230-01\week_13\ips-bad.tmp") | % `
+    { $_ -replace "^","iptables -A INPUT -s " -replace "$"," -j DROP" } | `
+    Out-File -FilePath "C:\Users\champuser\Desktop\CSI230-01\week_13\iptables.bash"
+}
+
+function Get-WindowsFirewall {
+    (Get-Content -Path "C:\Users\champuser\Desktop\CSI230-01\week_13\ips-bad.tmp") | % `
+    { $_ -replace "^","netsh advfirewall firewall add rule name=`"BLOCK IP ADDRESS - $_`" dir=in action=block remoteip=$_" } | `
+    Out-File -FilePath "C:\Users\champuser\Desktop\CSI230-01\week_13\windows-firewall.bash"
+}
+
+function Ask-User {
+    Write-Host "Choose which you would like to generate:"
+    Write-Host "[i]ptables"
+    Write-Host "[W]indows Firewall"
+    $choice = Read-Host "Make your choice"
+    switch ($choice) {
+        'i' {
+            Get-iptables
+        }
+        'W' {
+            Get-WindowsFirewall
+        }
+        default {
+            Write-Host "Invalid choice. Please try again."
+            Ask-User
+        }
+    }
+}
+
+Ask-User
